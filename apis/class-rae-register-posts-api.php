b742d6e0e3330c5d6a10204e5d96f210dc4908e7
Imran Sayed <imranhsayed@gmail.com>
2019-05-18 11:01:22 +0530

    Create a class for registering posts api

--- apis/class-rae-register-posts-api.php
+++ apis/class-rae-register-posts-api.php
@@ -4,3 +4,14 @@
  *
  * @package REST API ENDPOINT
  */
+
+class Rae_Register_Posts_Api {
+	/**
+	 * Rae_Register_Posts_Api constructor.
+	 */
+	function __construct() {
+
+	}
+}
+
+new Rae_Register_Posts_Api();
